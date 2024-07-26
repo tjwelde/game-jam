9176c6c27f51035a96e7e8fa6cba71f06311d30c
Timo Welde <tjwelde@gmail.com>
2024-07-26 20:19:59 +0200

    rendering order example

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import './style.css'
-import { Application, Assets, Container, Sprite } from 'pixi.js'
+import { Application, Container, Sprite, Texture, Text } from 'pixi.js'
 
 const app = new Application()
 
@@ -7,44 +7,54 @@ window.__PIXI_APP__ = app;
 
 await app.init({ width: 640, height: 360 })
 
-const container = new Container({
-  x:app.screen.width / 2,
-  y:app.screen.height / 2
+// Label showing scene graph hierarchy
+const label = new Text({
+  text:'Scene Graph:\n\napp.stage\n  ┗ A\n     ┗ B\n     ┗ C\n  ┗ D',
+  style:{fill: '#ffffff'},
+  position: {x: 300, y: 100}
 });
 
-app.stage.addChild(container);
+app.stage.addChild(label);
 
-// load the texture
-await Assets.load('assets/sample.png');
+// Helper function to create a block of color with a letter
+const letters = [];
+function addLetter(letter, parent, color, pos) {
+  const bg = new Sprite(Texture.WHITE);
+  bg.width = 100;
+  bg.height = 100;
+  bg.tint = color;
 
-// Create the 3 sprites, each a child of the last
-const sprites = [];
-let parent = container;
-for (let i = 0; i < 3; i++) {
-  let wrapper = new Container();
-  let sprite = Sprite.from('assets/sample.png');
-  sprite.anchor.set(0.5);
-  wrapper.addChild(sprite);
-  parent.addChild(wrapper);
-  sprites.push(wrapper);
-  parent = wrapper;
+  const text = new Text({
+    text:letter,
+    style:{fill: "#ffffff"}
+  });
+
+  text.anchor.set(0.5);
+  text.position = {x: 50, y: 50};
+
+  const container = new Container();
+  container.position = pos;
+  container.visible = false;
+  container.addChild(bg, text);
+  parent.addChild(container);
+
+  letters.push(container);
+  return container;
 }
 
-// Set all sprite's properties to the same value, animated over time
+// Define 4 letters
+let a = addLetter('A', app.stage, 0xff0000, {x: 100, y: 100});
+let b = addLetter('B', a,         0x00ff00, {x: 20,  y: 20});
+let c = addLetter('C', a,         0x0000ff, {x: 20,  y: 40});
+let d = addLetter('D', app.stage, 0xff8800, {x: 140, y: 100});
+
+// Display them over time, in order
 let elapsed = 0.0;
-app.ticker.add((delta) => {
-  elapsed += delta.deltaTime / 60;
-  const amount = Math.sin(elapsed);
-  const scale = 1.0 + 0.25 * amount;
-  const alpha = 0.75 + 0.25 * amount;
-  const angle = 40 * amount;
-  const x = 75 * amount;
-  for (let i = 0; i < sprites.length; i++) {
-    const sprite = sprites[i];
-    sprite.scale.set(scale);
-    sprite.alpha = alpha;
-    sprite.angle = angle;
-    sprite.x = x;
+app.ticker.add((ticker) => {
+  elapsed += ticker.deltaTime / 60.0;
+  if (elapsed >= letters.length) { elapsed = 0.0; }
+  for (let i = 0; i < letters.length; i ++) {
+    letters[i].visible = elapsed >= i;
   }
 });
 
