9d208b444247e8d84eee30418032a79076bdb144
Timo Welde <tjwelde@gmail.com>
2024-07-26 20:09:02 +0200

    make things move

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,21 @@ import { Application, Assets, Sprite } from 'pixi.js'
 
 const app = new Application()
 
+;(window as any).__PIXI_APP__ = app;
+
 await app.init({ width: 640, height: 360 })
 
-await Assets.load('./Assets/sample.png')
-const sprite = Sprite.from('./Assets/sample.png')
+await Assets.load('./assets/sample.png')
+const sprite = Sprite.from('./assets/sample.png')
 
 app.stage.addChild(sprite)
+
+let elapsed = 0.0;
+
+app.ticker.add((ticker) => {
+  elapsed += ticker.deltaTime;
+  sprite.x = 100.0 + Math.cos(elapsed/50.0) * 100.0;
+});
+
 
 document.body.appendChild(app.canvas)
