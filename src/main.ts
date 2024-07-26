fd9d61703d70ea5d3b53e6fd9d16a3c31f6d4a3f
dudleyneedham <dudleyneedham1993@gmail.com>
2024-07-26 19:52:54 +0200

    adding first sprite and asset folder

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import './style.css'
-import { Application } from 'pixi.js'
+import { Application, Assets, Sprite } from 'pixi.js'
 
-const app = new Application();
-await app.init({ width: 640, height: 360 });
-document.body.appendChild(app.canvas);
+const app = new Application()
+
+await app.init({ width: 640, height: 360 })
+
+await Assets.load('./Assets/sample.png')
+const sprite = Sprite.from('./Assets/sample.png')
+
+app.stage.addChild(sprite)
+
+document.body.appendChild(app.canvas)
