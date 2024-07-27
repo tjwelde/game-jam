88058fac49ac8e4bcaca3168413b44f1f3771236
Angelos Angelides <angelangelides@gmail.com>
2024-07-27 22:07:36 +0200

    chore: remove commented out platform

--- src/main.js
+++ src/main.js
@@ -21,7 +21,6 @@ await app.init({ width: 1200, height: 600 });
 const platform1 = new Graphics().rect(100, 550, 1100, 50).fill(0xCBC3E3);
 const platform2 = new Graphics().rect(700, 300, 250, 250).fill(0x800080);
 const platform3 = new Graphics().rect(1000, 450, 100, 100).fill(0xff0000);
-// const platform2 = new Graphics().rect(1000, 450, 100, 100).fill(0xff0000);
 
 app.stage.addChild(platform1);
 app.stage.addChild(platform2);
